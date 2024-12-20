84aab4d4e921c6ceb036b46db6b892d998c61da9
karabo phahsa <johndoe@example.com>
2024-12-20 08:15:51 +0200

    implemented the number choosing functionality

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -1,47 +1,17 @@
 import random
 
-ROCK = 'r'
-SCISSORS = 's'
-PAPER = 'p'
-emojis = { ROCK: '🪨', SCISSORS: '✂️', PAPER: '📃' }
-choices = tuple(emojis.keys())
-
-def get_user_choice():
-  while True:
-    user_choice = input('Rock, paper, or scissors? (r/p/s): ').lower()
-    if user_choice in choices:
-      return user_choice      
+while True:
+    choice = input("Rock , Paper or Scissors (r,p,s): ")
+    if choice not in "rps":
+        print("Invalid!") 
     else:
-      print('Invalid choice!')
-      
-def display_choices(user_choice, computer_choice):
-  print(f'You chose {emojis[user_choice]}')
-  print(f'Computer chose {emojis[computer_choice]}')
-
-
-def determine_winner(user_choice, computer_choice):
-  if user_choice == computer_choice:
-    print('Tie!')
-  elif (
-    (user_choice == ROCK and computer_choice == SCISSORS) or 
-    (user_choice == SCISSORS and computer_choice == PAPER) or 
-    (user_choice == PAPER and computer_choice == ROCK)):
-    print('You win')
-  else:
-    print('You lose')  
-
-def play_game():
-  while True:
-    user_choice = get_user_choice()
-
-    computer_choice = random.choice(choices)
-
-    display_choices(user_choice, computer_choice)
-
-    determine_winner(user_choice, computer_choice)
-
-    should_continue = input('Continue? (y/n): ').lower()
-    if should_continue == 'n':
-      break
-
-play_game()
+        print('valid!')
+        while True:
+          end_game = input('want to continue?: ')
+          if end_game == 'n':
+              print('Thank you for playing')
+              exit()
+          elif end_game == "y":
+              break
+          else: 
+              print("Invalid!")
