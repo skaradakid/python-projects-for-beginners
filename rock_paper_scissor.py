0fbe8b3f7779aa66d1d5497ab400b832e0918490
karabo phahsa <johndoe@example.com>
2024-12-20 08:38:22 +0200

    improved player choice, implemented cpu choices

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -1,13 +1,28 @@
 import random
 
+cpu_choices = ['Rock', 'Paper', 'Scissors']
+game = [{('rock', 'paper'): 'paper'}, {('rock', 'scissors'): "rock"}, {('scissors', 'paper'): 'scissors'}]
+
 while True:
-    choice = input("Rock , Paper or Scissors (r,p,s): ")
-    if choice not in "rps":
+
+    player_choice = input("Rock , Paper or Scissors (r,p,s): ")
+    if player_choice == "r":
+        player_choice = "Rock"
+    elif player_choice == "p":
+        player_choice = 'Paper'
+    elif player_choice == "s":
+        player_choice = "Scissors"
+    cpu_choice = random.choice(cpu_choices)
+    
+    if player_choice not in ["Rock", "Paper", "Scissors"]:
         print("Invalid!") 
     else:
-        print('valid!')
+        print(f'you chose {player_choice}')
+        print(f"cpu chose {cpu_choice}")
+        
         while True:
           end_game = input('want to continue?: ')
+          
           if end_game == 'n':
               print('Thank you for playing')
               exit()
